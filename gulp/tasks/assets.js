48c81001a9c26a5a06efa608156be886904f7ea5
Sashko Matviichuk <sashko1921@gmail.com>
2017-05-20 00:24:11 +0200

    Add notifications.

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,11 +1,21 @@
 const debug = require('gulp-debug');
+const notify = require('gulp-notify');
+const combiner = require('stream-combiner2').obj;
+
+const onError = err => ({
+    title: 'Assets build error',
+    message: err.message,
+});
 
 module.exports = (gulp, options) => {
     const { appPath } = options;
 
     if (!appPath) throw new Error('Parameter "appPath" is required for sass task');
 
-    return gulp.src(`${appPath}src/**/*.{html,svg,png,ico}`, { since: gulp.lastRun('assets') })
-        .pipe(debug({title: 'assets'}))
-        .pipe(gulp.dest(`${appPath}public`));
+    return combiner(
+        gulp.src(`${appPath}src/**/*.{html,svg,png,ico}`, { since: gulp.lastRun('assets') }),
+        debug({title: 'assets'}),
+        gulp.dest(`${appPath}public`)
+    )
+        .on('error', notify.onError(onError));
 };
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,16 +2,30 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const debug = require('gulp-debug');
+const notify = require('gulp-notify');
+const combiner = require('stream-combiner2').obj;
+
+const onError = err => ({
+    title: 'Sass compilation error',
+    message: err.message,
+});
 
 module.exports = (gulp, options) => {
     const { appPath, isDev } = options;
 
     if (!appPath) throw new Error('Parameter "appPath" is required for sass task');
 
-    return gulp.src(`${appPath}src/styles/**/*.sass`)
-        .pipe(gulpIf(isDev, sourcemaps.init()))
-        .pipe(debug({title: 'sass'}))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulpIf(isDev, sourcemaps.write()))
-        .pipe(gulp.dest(`${appPath}public/css`));
+    return combiner(
+        gulp.src(`${appPath}src/styles/**/*.sass`),
+        gulpIf(isDev, sourcemaps.init()),
+        debug({title: 'sass'}),
+        sass(),
+        gulpIf(isDev, sourcemaps.write()),
+        gulp.dest(`${appPath}public/css`),
+        notify({
+            message: 'Compiled successfuly',
+            title: 'Sass'
+        })
+    )
+        .on('error', notify.onError(onError));
 };
